5f3e313ab104c799371e54d5ad901a810f04d1d5
pixelsurge <sahan.user@gmail.com>
2024-08-08 05:29:33 +0530

    kde config homemanager

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -15,12 +19,15 @@
     nixpkgs,
     ## nixpkgs-unstable,
     home-manager,
+    plasma-manager,
     ...
   } @ inputs: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    username = "sahan";
     system = "x86_64-linux";
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
       /*
         specialArgs = {
         pkgs-stable = import nixpkgs-unstable {
@@ -36,6 +43,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.sharedModules = [plasma-manager.homeManagerModules.plasma-manager];
           home-manager.users.sahan = import ./home-manager.nix;
         }
       ];
--- home-manager.nix
+++ home-manager.nix
@@ -7,6 +7,7 @@
   imports = [
     ./applications/alacritty.nix
     ./applications/fish.nix
+    ./applications/plasma.nix
   ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
