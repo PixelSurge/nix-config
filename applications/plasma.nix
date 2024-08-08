c3cb294e9a40f0e84c9dcd0f8bc31d42a4cd316d
pixelsurge <sahan.user@gmail.com>
2024-08-08 06:17:57 +0530

    fixes

--- applications/plasma.nix
+++ applications/plasma.nix
@@ -8,9 +8,7 @@
   programs.plasma = {
     enable = true;
     workspace = {
-      clickItemTo = "open"; # If you liked the click-to-open default from plasma 5
       lookAndFeel = "org.kde.breezedark.desktop";
-      iconTheme = "Breeze";
       wallpaper = "/home/sahan/nix-config/img.jpg";
     };
   };
